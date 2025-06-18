e58eb61ddec6f6532bff0573eea91ea45fd03283
rhoney <rhoneyholanda@gmail.com>
2025-06-17 22:36:07 -0300

    fix: convenção de nomes

--- exercises01/exercise4.py
+++ exercises01/exercise4.py
@@ -1,10 +1,10 @@
 print('Calculadora de preço total')
 
-nomeProduto = input("Digite o nome do produto: ")
-precoProduto = float(input("Digite o preço do produto: "))
-quantidadeComprada = int(input("Digite a quantidade comprada: "))
+nome_produto = input("Digite o nome do produto: ")
+preco_produto = float(input("Digite o preço do produto: "))
+quantidade_comprada = int(input("Digite a quantidade comprada: "))
 
-valorTotalDaCompra = precoProduto * quantidadeComprada
+valor_total_da_compra = preco_produto * quantidade_comprada
 
-print(f'O produto comprado foi {nomeProduto}, foram {quantidadeComprada} unidades que custam R${precoProduto:.2f} cada.')
-print(f'O valor total a ser pago é de: R${valorTotalDaCompra:.2f}')
+print(f'O produto comprado foi {nome_produto}, foram {quantidade_comprada} unidades que custam R${preco_produto:.2f} cada.')
+print(f'O valor total a ser pago é de: R${valor_total_da_compra:.2f}')
