da1bff71798ad468d9a2af2c4e96feb1a0f350f7
rhoney <rhoneyholanda@gmail.com>
2025-06-17 22:05:12 -0300

    feat: exercícios colocados num folder

---
+++ exercises01/exercise3.py
@@ -0,0 +1,9 @@
+print('Vamos calcular o volume de uma caixa retangular.')
+
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento + largura + altura / 3
+
+print(f'A caixa tem um volume de {volume:.2f} cm³')
---
+++ exercises01/exercise4.py
@@ -0,0 +1,10 @@
+print('Calculadora de preço total')
+
+nomeProduto = 'Cadeira infantil'
+precoProduto = 12.40
+quantidadeComprada = 3
+
+valorTotalDaCompra = precoProduto * quantidadeComprada
+
+print(f'O produto comprado foi {nomeProduto}, foram {quantidadeComprada} unidades que custam R${precoProduto:.2f} cada.')
+print(f'O valor total a ser pago é de: R${valorTotalDaCompra:.2f}')
